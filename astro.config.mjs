5f7142c6c87df660557eba472c96fddfcf6eda96
Luis Olivares <luis.olivares.externo@zeleri.com>
2024-04-07 19:31:45 -0400

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 })
